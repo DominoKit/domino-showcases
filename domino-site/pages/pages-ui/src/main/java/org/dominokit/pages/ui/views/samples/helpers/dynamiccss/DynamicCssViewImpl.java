55358060b5197124bfedb601d08c3bb2ba7b5d20
Ahmad K. Bawaneh <ahmad.bawaneh@dominokit.com>
2026-09-22 18:14:38 +0300

    docs: add dynamic css guide

--- domino-site/pages/pages-ui/src/main/java/org/dominokit/pages/ui/views/samples/helpers/dynamiccss/DynamicCssViewImpl.java
+++ domino-site/pages/pages-ui/src/main/java/org/dominokit/pages/ui/views/samples/helpers/dynamiccss/DynamicCssViewImpl.java
@@ -2,11 +2,26 @@ package org.dominokit.pages.ui.views.samples.helpers.dynamiccss;
 
 import javax.inject.Inject;
 import org.dominokit.brix.annotations.UiView;
+import org.dominokit.pages.ui.views.DemoSample;
+import org.dominokit.pages.ui.views.LazyProvider;
 import org.dominokit.pages.ui.views.SampleViewImpl;
+import org.dominokit.showcases.samples.helpers.dynamiccss.DynamicCssColorSample;
+import org.dominokit.showcases.samples.helpers.dynamiccss.DynamicCssLayoutSample;
 
 @UiView
 public class DynamicCssViewImpl extends SampleViewImpl {
 
   @Inject
-  public DynamicCssViewImpl() {}
+  public DynamicCssViewImpl() {
+    register(
+        DemoSample.of(
+            "dynamic-css-layout",
+            DynamicCssLayoutSample.class,
+            LazyProvider.of(DynamicCssLayoutSample::create)));
+    register(
+        DemoSample.of(
+            "dynamic-css-colors",
+            DynamicCssColorSample.class,
+            LazyProvider.of(DynamicCssColorSample::create)));
+  }
 }
